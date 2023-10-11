a2e7dbf21eeecdf3a5ff4150a65c645019129188
abdoufekkak <abdou.fekkak66@gmail.com>
2023-10-11 19:31:34 +0200

    wer

---
+++ src/app/profile/componets/poster-postes/poster-postes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poster-postes',
+  templateUrl: './poster-postes.component.html',
+  styleUrls: ['./poster-postes.component.scss']
+})
+export class PosterPostesComponent {
+
+}
---
+++ src/app/profile/componets/postes/postes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postes',
+  templateUrl: './postes.component.html',
+  styleUrls: ['./postes.component.scss']
+})
+export class PostesComponent {
+
+}
