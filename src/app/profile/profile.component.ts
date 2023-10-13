8c37e202b47cbc1eb1c3791b1eb44b9825737860
Salma-Raf <rksalma6@gmail.com>
2023-10-13 18:53:48 +0100

    mm

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,21 +1,21 @@
-import { Component ,OnInit} from '@angular/core';
-import { postService } from './service/post.service';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './service/post.service';
 import { Post } from './model/post.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent  implements OnInit{
-  id_sender!:number
-  posts!:Post[]
-  constructor(public servicePost:postService){
-
-  }
+export class ProfileComponent implements OnInit {
+  id_sender!: number;
+  posts!: Post[];
+  constructor(public servicePost: PostService) {}
   ngOnInit(): void {
-    this.id_sender =parseInt(localStorage.getItem('idUser') as string, 10);
-    this.servicePost.GetAllpost(this.id_sender).subscribe(e=>this.posts=e,err=>console.log(err));
+    this.id_sender = parseInt(localStorage.getItem('idUser') as string, 10);
+    this.servicePost.GetAllpost(this.id_sender).subscribe(
+      (e) => (this.posts = e),
+      (err) => console.log(err)
+    );
   }
-
 }
