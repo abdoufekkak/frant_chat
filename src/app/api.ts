32ce17a2eae8d14466d3f3264475f812fbb04a24
abdoufekkak <abdou.fekkak66@gmail.com>
2023-10-14 17:04:10 +0200

    Wert

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-export const host='10.0.0.139'
+export const host='localhost'
 export const api=`http://${host}:3000/api`
-=======
-export const host = '10.0.0.139';
-export const api = `http://${host}:3000/api`;
->>>>>>> b24ebfda4e24a3007fd8b3c2473da46e17d9fb4e
 
 export const api2 = `http://${host}:3001/api`;
